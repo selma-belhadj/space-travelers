42a95b359cf145789c8acfe55fb729ec2a385fc0
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-04-05 18:57:57 +0200

    Render list of dragons in dragons route; add basic style

--- src/pages/Dragons.js
+++ src/pages/Dragons.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { listDragons } from '../redux/dragons/dragons';
+import Dragon from '../components/Dragon';
+import './dragons.scss';
 
-function Dragons() {
+const Dragons = () => {
+  const dispatch = useDispatch();
+  const dragonsList = useSelector((state) => state.dragons);
+
+  useEffect(() => {
+    if (dragonsList.length < 1) {
+      dispatch(listDragons());
+    }
+  });
+
   return (
-    <div>Dragons</div>
+    <div className="content dragons-page-container">
+      <ul>
+        {dragonsList.map((dragon) => (
+          <Dragon
+            key={dragon.dragonId}
+            dragonId={dragon.dragonId}
+            dragonName={dragon.dragonName}
+            dragonType={dragon.dragonType}
+            dragonDescription={dragon.dragonDescription}
+            dragonImageLink={dragon.dragonImage}
+            dragonReserved={dragon.dragonReserved}
+          />
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default Dragons;
+
+// const DragonsList = () => {
+//   const dispatch = useDispatch();
+//   const dragonsList = useSelector((state) => state.dragons);
+
+//   useEffect(() => {
+//     if (dragonsList.length < 1) {
+//       dispatch(listDragons());
+//     }
+//   });
+
+//   return (
+//     <ul>
+//       {dragonsList.map((dragon) => (
+//         <DragonItem
+//           key={dragon.dragonId}
+//           dragonId={dragon.dragonId}
+//           dragonName={dragon.dragonName}
+//           dragonType={dragon.dragonType}
+//           dragonDescription={dragon.dragonDescription}
+//           dragonImageLink={dragon.dragonImage}
+//           dragonReserved={dragon.dragonReserved}
+//         />
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default DragonsList;
