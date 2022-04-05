b940bea9e2a5e0fb96edcf5319a67a46838bcad3
Awais Amjed <awaisamjed171@gmail.com>
2022-04-05 16:12:13 +0500

    Add redux store functionality to fetch all rockets and save data to store

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,3 +1,22 @@
-const rocketsReducer = () => {};
+import SpaceXAPI from '../../api/spaceXAPI';
+
+const ROCKETS_FETCHED = 'ROCKETS_FETCHED';
+
+const fetchAllRockets = () => async (dispatch) => {
+  const rockets = await SpaceXAPI.fetchAllRockets();
+  if (rockets) {
+    dispatch({ type: ROCKETS_FETCHED, rockets });
+  }
+  // TODO Show API Error
+};
+
+const rocketsReducer = (state = [], actions) => {
+  switch (actions.type) {
+    case ROCKETS_FETCHED:
+      return actions.rockets;
+    default:
+      return state;
+  }
+};
 
 export default rocketsReducer;
