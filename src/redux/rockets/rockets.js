3014aaa23f360ebbf44f699ccc203adb749595d7
Awais Amjed <awaisamjed171@gmail.com>
2022-04-05 21:22:28 +0500

    Add Toggle reserved status functionality

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,24 +1,39 @@
 import SpaceXAPI from '../../api/spaceXAPI';
 
 const ROCKETS_FETCHED = 'ROCKETS_FETCHED';
+const TOGGLE_RESERVED = 'TOGGLE_RESERVED';
 
 const fetchAllRockets = () => async (dispatch) => {
   const rockets = await SpaceXAPI.fetchAllRockets();
   if (rockets) {
     dispatch({ type: ROCKETS_FETCHED, rockets });
   }
-  // TODO Show API Error
 };
+
+const toggleReservedStatus = ({ id }) => ({
+  type: TOGGLE_RESERVED,
+  id,
+});
 
 const rocketsReducer = (state = [], actions) => {
   switch (actions.type) {
     case ROCKETS_FETCHED:
       return actions.rockets;
+    case TOGGLE_RESERVED:
+      return state.map((rocket) => {
+        if (rocket.id === actions.id) {
+          return {
+            ...rocket,
+            reserved: rocket.reserved ? !rocket.reserved : true,
+          };
+        }
+        return rocket;
+      });
     default:
       return state;
   }
 };
 
 export {
-  rocketsReducer as default, fetchAllRockets,
+  rocketsReducer as default, fetchAllRockets, toggleReservedStatus,
 };
