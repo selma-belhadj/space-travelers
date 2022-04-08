ff478170d0b30ad09ca9b7c7a8d5b1252e37a1d5
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-04-08 03:21:46 +0200

    Add tests for dragons page

--- src/test/pages/dragons.test.js
+++ src/test/pages/dragons.test.js
@@ -1,71 +1,89 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-// import { act } from "react-dom/test-utils";
+import { act } from "react-dom/test-utils";
+import Dragons from '../../pages/Dragons';
 import store from '../../redux/configureStore';
-import Dragon from '../../components/Dragon'
+import * as abc from '../../redux/dragons/dragons';
 
-describe('Dragon', () => {
-  test('renders Dragon component correctly - when not reserved', () => {
+const fakeDragons = [
+  {
+    dragonId: '0',
+    dragonName: 'Dragon 1',
+    dragonType: 'capsule',
+    dragonDescription: 'Test Dragon 1',
+    dragonImage: 'img_url_1',
+    dragonReserved: false,
+  },
+  {
+    dragonId: '1',
+    dragonName: 'Dragon 2',
+    dragonType: 'capsule',
+    dragonDescription: 'Test Dragon 2',
+    dragonImage: 'img_url_2',
+    dragonReserved: true,
+  },
+];
+
+const mockListDragons = () => {
+  jest.spyOn(abc, "listDragons").mockImplementation(() => (dispatch) => {
+    dispatch({
+      type: abc.LIST_DRAGONS,
+      payload: fakeDragons,
+    });
+  });
+};
+
+describe('Testing Dragons Page', () => {
+  test('renders list items correctly', () => {
+    mockListDragons();
     render(
       <Provider store={store}>
-        <Dragon
-          key='01'
-          dragonId='01'
-          dragonName='FireDragon'
-          dragonType='capsule'
-          dragonDescription='huge fire dragon'
-          dragonImageLink='image_link'
-          dragonReserved={false}
-        />
-      </Provider>
+        <Dragons />
+      </Provider>,
     );
-    // title to match dragonName
-    expect(screen.getByRole('heading').textContent).toBe('FireDragon');
-    // image source url to match passed dragonImageLink
-    expect(screen.getByRole('img').src).toContain('image_link');
-    // dragonType and dragonDescription to be in document
-    expect(screen.getByText(/capsule/)).toBeInTheDocument();
-    expect(screen.getByText(/huge fire dragon/)).toBeInTheDocument();
-    // check button and badge conditional rendering
-    expect(screen.getByRole('button').textContent).toBe('Reserve Dragon');
-    expect(screen.queryByText(/Reserved/)).toBeNull();
-    expect(screen.queryByText(/Cancel Reservation/)).toBeNull();;
+    const listItems = screen.getAllByRole('listitem');
+    // should render 2 list items
+    expect(listItems.length).toBe(2);
+    // Titles
+    expect(screen.getAllByRole('heading')[0].textContent).toBe('Dragon 1');
+    expect(screen.getAllByRole('heading')[1].textContent).toBe('Dragon 2');
+    // images urls
+    expect(screen.getAllByRole('img')[0].src).toContain('img_url_1');
+    expect(screen.getAllByRole('img')[1].src).toContain('img_url_2');
+    // types and descriptions
+    expect(screen.getAllByText(/capsule/).length).toBe(2);
+    expect(screen.getByText(/Test Dragon 1/)).toBeInTheDocument();
+    expect(screen.getByText(/Test Dragon 2/)).toBeInTheDocument();
+    // buttons have correct text content
+    expect(screen.getAllByRole('button')[0].textContent).toBe('Reserve Dragon');
+    expect(screen.getAllByRole('button')[1].textContent).toBe('Cancel Reservation');
+    // badge RESERVED is displayed only on reserved item
+    const badge = screen.queryByText(/Reserved/);
+    expect(screen.getAllByRole('listitem')[0]).not.toContainElement(badge);
+    expect(screen.getAllByRole('listitem')[1]).toContainElement(badge);
   });
 
-  test('renders Dragon component correctly - when reserved', () => {
+  test('buttons change text when clicked', () => {
+    mockListDragons();
     render(
       <Provider store={store}>
-        <Dragon
-          key='02'
-          dragonId='02'
-          dragonName='Darksmoke'
-          dragonType='capsule1'
-          dragonDescription='description'
-          dragonImageLink='image_link1'
-          dragonReserved={true}
-        />
-      </Provider>
+        <Dragons />
+      </Provider>,
     );
-    // title to match dragonName
-    expect(screen.getByRole('heading').textContent).toBe('Darksmoke');
-    // image source url to match passed dragonImageLink
-    expect(screen.getByRole('img').src).toContain('image_link1');
-    // dragonType and dragonDescription to be in document
-    expect(screen.getByText(/capsule1/)).toBeInTheDocument();
-    expect(screen.getByText(/description/)).toBeInTheDocument();
-    // check button and badge conditional rendering
-    expect(screen.getByRole('button').textContent).toBe('Cancel Reservation');
-    expect(screen.queryByText(/Reserved/)).toBeInTheDocument();
-    expect(screen.queryByText(/Reserve Dragon/)).toBeNull();
+    const btn1 = screen.getAllByRole('button')[0];
+    const btn2 = screen.getAllByRole('button')[1];
+    //before clicking
+    expect(btn1.textContent).toBe('Reserve Dragon');
+    expect(btn2.textContent).toBe('Cancel Reservation');
 
-    // const btn = screen.getByRole('button');
-    // console.log('before button clicked', btn.innerHTML);
-    // act(() => {
-    //   btn.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-    // });
+    // click event
+    act(() => {
+      btn1.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+      btn2.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+    });
 
-    // console.log('button clicked', btn.innerHTML);
+    expect(btn1.textContent).toBe('Cancel Reservation');
+    expect(btn2.textContent).toBe('Reserve Dragon');
   });
-
 });
